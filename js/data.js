f7e0cefa73c5105496ac6786f5cacd732953e8ad
Jullka <dj-1@ukr.net>
2019-11-09 00:13:36 +0200

    Добавляет задание Больше деталей

--- js/data.js
+++ js/data.js
@@ -27,6 +27,45 @@
     similarListElement.appendChild(fragment);
   };
 
+  var bigPicture = document.querySelector('.big-picture');
+  bigPicture.classList.remove('hidden');
+  var commentsList = document.querySelector('.social__comments');
+  var comment = document.querySelectorAll('.social__comment');
+  var commentCountElement = document.querySelector('.social__comment-count');
+  var commentsLoader = document.querySelector('.comments-loader');
+
+  var renderBigPicture = function (picture) {
+    var bigPictureCommentsCount = picture.comments.length;
+    bigPicture.querySelector('big-picture__img').querySelector('img').src = picture.url;
+    bigPicture.querySelector('.likes-count').textContent = picture.likes;
+    bigPicture.querySelector('.comments-count').textContent = bigPictureCommentsCount;
+    bigPicture.querySelector('.social__caption').textContent = picture.description;
+
+
+    for (var i = 0; i < comment.length; i++) {
+      commentsList.removeChild(commentsList[i]);
+    }
+
+    var newSocialCommentsElements = Array(bigPictureCommentsCount);
+    var socialElementFragment = document.createDocumentFragment();
+
+    for (var j = 0; j < bigPictureCommentsCount; j++) {
+      newSocialCommentsElements[j] = comment[0].cloneNode(true);
+      newSocialCommentsElements[j].querySelector('img').src = picture.comments[j].avatar;
+      newSocialCommentsElements[j].querySelector('img').alt = picture.comments[j].name;
+      newSocialCommentsElements[j].querySelector('.social__text').textContent = picture.comments[j].message;
+
+      socialElementFragment.appendChild(newSocialCommentsElements[j]);
+    }
+
+    comment.appendChild(socialElementFragment);
+
+    commentsLoader.classList.add('.visually-hidden');
+    commentCountElement.classList.add('.visually-hidden');
+  };
+  var elements = renderPicture();
+  renderBigPicture(elements[0]);
+
   var onError = function (errorText) {
     var errorElement = errorTemplate.cloneNode(true);
     errorElement.querySelector('.error__title').textContent = errorText;
