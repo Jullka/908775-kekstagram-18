d62a48a932b624eb62757b392de91e5e832ec6ec
Jullka <dj-1@ukr.net>
2019-11-09 21:08:10 +0200

    Добавляет исправления

--- js/data.js
+++ js/data.js
@@ -30,7 +30,7 @@
   var bigPicture = document.querySelector('.big-picture');
   bigPicture.classList.remove('hidden');
   var commentsList = document.querySelector('.social__comments');
-  var comment = document.querySelectorAll('.social__comment');
+  var commentElement = document.querySelectorAll('.social__comment');
   var commentCountElement = document.querySelector('.social__comment-count');
   var commentsLoader = document.querySelector('.comments-loader');
 
@@ -42,26 +42,25 @@
     bigPicture.querySelector('.social__caption').textContent = picture.description;
 
 
-    for (var i = 0; i < comment.length; i++) {
+    for (var i = 0; i < commentElement.length; i++) {
       commentsList.removeChild(commentsList[i]);
     }
 
-    var newSocialCommentsElements = Array(bigPictureCommentsCount);
     var socialElementFragment = document.createDocumentFragment();
 
     for (var j = 0; j < bigPictureCommentsCount; j++) {
-      newSocialCommentsElements[j] = comment[0].cloneNode(true);
-      newSocialCommentsElements[j].querySelector('img').src = picture.comments[j].avatar;
-      newSocialCommentsElements[j].querySelector('img').alt = picture.comments[j].name;
-      newSocialCommentsElements[j].querySelector('.social__text').textContent = picture.comments[j].message;
+      var comment = commentElement.cloneNode(true);
+      comment.querySelector('img').src = picture.comments[j].avatar;
+      comment.querySelector('img').alt = picture.comments[j].name;
+      comment.querySelector('.social__text').textContent = picture.comments[j].message;
 
-      socialElementFragment.appendChild(newSocialCommentsElements[j]);
+      socialElementFragment.appendChild(comment);
     }
 
-    comment.appendChild(socialElementFragment);
+    commentElement.appendChild(socialElementFragment);
 
-    commentsLoader.classList.add('.visually-hidden');
-    commentCountElement.classList.add('.visually-hidden');
+    commentsLoader.classList.add('visually-hidden');
+    commentCountElement.classList.add('visually-hidden');
   };
   var elements = renderPicture();
   renderBigPicture(elements[0]);
--- js/util.js
+++ js/util.js
@@ -1,11 +1,13 @@
 'use strict';
 
 (function () {
-  var ESC_KEYCODE = 27;
-  var ENTER_KEYCODE = 13;
+  var KEYCODE = {
+    ESC: 27,
+    ENTER: 13
+  };
 
-  window.util = {
-    ESC_KEYCODE: ESC_KEYCODE,
-    ENTER_KEYCODE: ENTER_KEYCODE,
+  window.KEYCODE = {
+    ESC: KEYCODE.ESC,
+    ENTER: KEYCODE.ENTER
   };
 })();
